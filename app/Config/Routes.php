aecceb69d2ed9652da9ce50f4c6b57e382793aae
Lua <130156447+Mr-LuaM@users.noreply.github.com>
2023-09-30 00:45:07 +0800

    V0.2
    AUTH

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'ProductsController::index');
-$routes->get('register', 'MainController::showRegistrationForm');
-$routes->get('viewProducts/(:any)', 'ProductsController::viewProducts/$1');
-$routes->get('product', 'ProductsController::products');
+$routes->get('register', 'MainController::register');
+$routes->post('registration', 'MainController::registration');
+$routes->get('login', 'MainController::login');
+$routes->post('loginAuth', 'MainController::loginAuth');
+$routes->get('viewProducts/(:any)', 'ProductsController::viewProducts/$1', ['filter' => 'authGuard']);
+$routes->get('product', 'ProductsController::products', ['filter' => 'authGuard']);
--- app/Views/register.php
+++ app/Views/register.php
@@ -24,7 +24,7 @@
 
               <!-- Display validation errors if any -->
               <?php if (isset($validation)): ?>
-                <div class="alert alert-warning">
+                <div class="alert alert-danger">
                   <ul>
                     <?php foreach ($validation->getErrors() as $error): ?>
                       <li>
@@ -35,32 +35,35 @@
                 </div>
               <?php endif; ?>
 
-              <form action="<?= base_url('SignupController/store') ?>" method="post">
+
+              <form action="<?= base_url() ?>registration" method="post">
                 <div class="input-box">
                   <div class="single-input-fields">
-                    <label>Full name</label>
-                    <input type="text" name="name" placeholder="Enter full name" value="<?= old('name') ?>"
-                      class="form-control">
+                    <label>Username</label>
+                    <input type="text" name="username" placeholder="Enter full name" value="<?= old('name') ?>"
+                      class="form-control" required>
                   </div>
                   <div class="single-input-fields">
                     <label>Email Address</label>
                     <input type="email" name="email" placeholder="Enter email address" value="<?= old('email') ?>"
-                      class="form-control">
+                      class="form-control" required>
                   </div>
                   <div class="single-input-fields">
                     <label>Password</label>
-                    <input type="password" name="password" placeholder="Enter Password" class="form-control">
+                    <input type="password" name="password" placeholder="Enter Password" class="form-control" required>
                   </div>
                   <div class="single-input-fields">
                     <label>Confirm Password</label>
-                    <input type="password" name="confirmpassword" placeholder="Confirm Password" class="form-control">
+                    <input type="password" name="confirmpassword" placeholder="Confirm Password" class="form-control"
+                      required>
                   </div>
                 </div>
                 <div class="register-footer">
-                  <p> Already have an account? <a href="<?= base_url('login') ?>"> Login</a> here</p>
+                  <p> Already have an account? <a href="<?= base_url() ?>login"> Login</a> here</p>
                   <button type="submit" class="submit-btn3">Sign Up</button>
                 </div>
               </form>
+
             </div>
           </div>
         </div>
